311ddd353245c576682ab8f9f89cf68d1c17121e
humanra24 <56342494+humanra24@users.noreply.github.com>
2021-09-03 10:00:24 +0700

    Update facebook.php

--- SSO/application/config/facebook.php
+++ SSO/application/config/facebook.php
@@ -1,6 +1,6 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
  
-$config['facebook']['app_id'] = '4312223202207959'; //masukkan app id disini
-$config['facebook']['app_secret'] = 'cc27384acf609993f0e4c60c30e685ae'; //masukkan app secret disini
+$config['facebook']['app_id'] = 'xxx'; //masukkan app id disini
+$config['facebook']['app_secret'] = 'xxx'; //masukkan app secret disini
 $config['facebook']['default_graph_version'] = 'v2.10';
